2e6a30b60b96ebce920ff0268c1b87e5f64de2a2
21herbertm <53823046+21herbertm@users.noreply.github.com>
2023-07-27 11:54:39 -0400

    Added Inventory Page

--- FridgeVision/ContentView.swift
+++ FridgeVision/ContentView.swift
@@ -13,75 +13,167 @@ struct ContentView: View {
     @State private var isScanning = false
 
     var body: some View {
-        NavigationView {
-            VStack(spacing: 0) {
-                Color(UIColor.systemMint)
-                    .frame(height: 120)
-                    .edgesIgnoringSafeArea(.top)
+        TabView {
+            NavigationView {
+                VStack(spacing: 0) {
+                    Color(UIColor.systemMint)
+                        .frame(height: 120)
+                        .edgesIgnoringSafeArea(.top)
 
-                GeometryReader { geometry in
-                    VStack {
-                        if bluetoothManager.discoveredDevices.isEmpty {
-                            Spacer()
+                    GeometryReader { geometry in
+                        VStack {
+                            if bluetoothManager.discoveredDevices.isEmpty {
+                                Spacer()
 
-                            Text("No devices found")
-                                .frame(maxWidth: .infinity)
-                                .font(.headline)
-                                .padding()
-                                .background(Color.white)
-                                .foregroundColor(.black)
-                                .cornerRadius(10)
-                                .padding()
-                                .shadow(radius: 4)
+                                Text("No devices found")
+                                    .frame(maxWidth: .infinity)
+                                    .font(.headline)
+                                    .padding()
+                                    .background(Color.white)
+                                    .foregroundColor(.black)
+                                    .cornerRadius(10)
+                                    .padding()
+                                    .shadow(radius: 4)
 
-                            Spacer()
-                        } else {
-                            List(Array(bluetoothManager.discoveredDevices.enumerated()), id: \.element.identifier) { (index, device) in
-                                NavigationLink(destination: DeviceDetailView(device: device)
-                                    .environmentObject(bluetoothManager)
-                                ) {
-                                    Text(device.name ?? "Unknown Device")
+                                Spacer()
+                            } else {
+                                List(Array(bluetoothManager.discoveredDevices.enumerated()), id: \.element.identifier) { (index, device) in
+                                    NavigationLink(destination: DeviceDetailView(device: device)
+                                        .environmentObject(bluetoothManager)
+                                    ) {
+                                        Text(device.name ?? "Unknown Device")
+                                    }
                                 }
                             }
-                        }
 
-                        NavigationLink(destination: VideoStreamingView()) {
-                            Text("Live Stream")
-                                .padding()
-                                .background(Color.blue)
-                                .foregroundColor(.white)
-                                .cornerRadius(10)
-                                .padding()
-                                .shadow(radius: 4)
-                        }
-
-                        Button(action: {
-                            if isScanning {
-                                bluetoothManager.stopScanning()
-                            } else {
-                                bluetoothManager.startScanning()
+                            NavigationLink(destination: VideoStreamingView()) {
+                                Text("Live Stream")
+                                    .padding()
+                                    .background(Color.blue)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(10)
+                                    .padding()
+                                    .shadow(radius: 4)
                             }
-                            isScanning.toggle()
-                        }) {
-                            Text(isScanning ? "Stop Scanning" : "Scan for Devices")
-                                .padding()
-                                .background(Color.blue)
-                                .foregroundColor(.white)
-                                .cornerRadius(10)
-                                .padding()
-                                .shadow(radius: 4)
+                            
+                            Button(action: {
+                                if isScanning {
+                                    bluetoothManager.stopScanning()
+                                } else {
+                                    bluetoothManager.startScanning()
+                                }
+                                isScanning.toggle()
+                            }) {
+                                Text(isScanning ? "Stop Scanning" : "Scan for Devices")
+                                    .padding()
+                                    .background(Color.blue)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(10)
+                                    .shadow(radius: 4)
+                            }
+                            .padding(.bottom, 100)
                         }
+                        .padding()
                     }
-                    .padding()
+                }
+                .background(Color.white)
+                .edgesIgnoringSafeArea(.bottom)
+                .navigationTitle("")
+                .onAppear {
+                    bluetoothManager.startScanning()
                 }
             }
-            .background(Color.white)
-            .edgesIgnoringSafeArea(.bottom)
-            .navigationTitle("")
-            .onAppear {
-                bluetoothManager.startScanning()
+            .environmentObject(bluetoothManager)
+            .tabItem {
+                Image(systemName: "1.circle")
+                Text("Bluetooth")
             }
+            
+            InventoryView()
+                .tabItem {
+                    Image(systemName: "2.circle")
+                    Text("Inventory")
+                }
         }
-        .environmentObject(bluetoothManager)
+    }
+}
+
+struct InventoryView: View {
+    @State private var inventory: [(item: String, quantity: Int)] = []
+    @State private var addItemPresented = false
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("CURRENT INVENTORY")
+                    .font(.largeTitle)
+                    .padding()
+                    
+                List {
+                    ForEach(inventory.indices, id: \.self) { index in
+                        HStack {
+                            Text(inventory[index].item)
+                            Spacer()
+                            Text("\(inventory[index].quantity)")
+                        }
+                    }
+                    .onDelete { indexSet in
+                        inventory.remove(atOffsets: indexSet)
+                    }
+                }
+                
+                Button(action: {
+                    addItemPresented.toggle()
+                }) {
+                    HStack {
+                        Image(systemName: "plus")
+                        Text("ADD FOOD")
+                    }
+                }
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .padding()
+                .sheet(isPresented: $addItemPresented) {
+                    AddItemView { item, quantity in
+                        inventory.append((item: item, quantity: quantity))
+                        addItemPresented = false
+                    }
+                }
+            }
+            .navigationTitle("Inventory")
+            .navigationBarItems(leading: EditButton())
+        }
+    }
+}
+
+struct AddItemView: View {
+    @Environment(\.dismiss) var dismiss
+    @State private var itemName = ""
+    @State private var itemQuantity = 1
+    
+    var addItem: (String, Int) -> Void
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("Item")) {
+                    TextField("Name", text: $itemName)
+                    Stepper("Quantity: \(itemQuantity)", value: $itemQuantity, in: 1...100)
+                }
+                
+                Button("Add Item") {
+                    addItem(itemName, itemQuantity)
+                }
+            }
+            .navigationTitle("Add Item")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
