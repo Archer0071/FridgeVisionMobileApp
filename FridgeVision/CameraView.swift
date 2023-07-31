aec4434fa39b2e57ff49bab101d950080df71fbe
archer <adilking0071@gmail.com>
2023-07-31 22:59:18 +0500

    fixed some of the code now objects are being detected

--- FridgeVision/CameraView.swift
+++ FridgeVision/CameraView.swift
@@ -12,59 +12,71 @@ import Vision
 
 struct CameraView: UIViewRepresentable {
     private var captureSession = AVCaptureSession()
-
+    @State private var detectedObjects: [String] = []
+    
+    
     init() {
         // Set up the camera session configuration here
     }
-
+    
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
-
+    
     func makeUIView(context: Context) -> UIView {
         guard let captureDevice = AVCaptureDevice.default(for: .video) else { return UIView() }
-        let deviceInput = try! AVCaptureDeviceInput(device: captureDevice)
-
-        if captureSession.canAddInput(deviceInput) {
-            captureSession.addInput(deviceInput)
+        do {
+            let deviceInput = try AVCaptureDeviceInput(device: captureDevice)
+            
+            if captureSession.canAddInput(deviceInput) {
+                captureSession.addInput(deviceInput)
+            }
+        } catch {
+            print("Error: \(error.localizedDescription)")
+            return UIView()
         }
-
+        
         // Video data output
         let videoOutput = AVCaptureVideoDataOutput()
         videoOutput.setSampleBufferDelegate(context.coordinator, queue: DispatchQueue(label: "videoQueue"))
         if captureSession.canAddOutput(videoOutput) {
             captureSession.addOutput(videoOutput)
         }
-
-        captureSession.startRunning()
-
+        DispatchQueue.global(qos: .background).async {
+            captureSession.startRunning()
+        }
+        
+        
         let view = UIView()
         view.backgroundColor = UIColor.black
-
+        
         let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
         previewLayer.videoGravity = .resizeAspectFill
         view.layer.addSublayer(previewLayer)
-
+        
         DispatchQueue.main.async {
             previewLayer.frame = view.bounds
         }
-
+        
+        // Store the previewLayer in the coordinator
+        context.coordinator.previewLayer = previewLayer
+        
         return view
     }
-
+    
     func updateUIView(_ uiView: UIView, context: Context) {
-        if let previewLayer = uiView.layer.sublayers?[0] as? AVCaptureVideoPreviewLayer {
+        if let previewLayer = context.coordinator.previewLayer {
             previewLayer.frame = uiView.bounds
         }
     }
-
+    
     class Coordinator: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate {
         var parent: CameraView
-        let model: VNCoreMLModel
-        
+        var previewLayer: AVCaptureVideoPreviewLayer?
+        let model = try! VNCoreMLModel(for: ObjectDetector(configuration: MLModelConfiguration()).model)
         init(_ parent: CameraView) {
             self.parent = parent
-            self.model = try! VNCoreMLModel(for: SeeFood().model)
+            
             super.init()
         }
         
@@ -76,20 +88,23 @@ struct CameraView: UIViewRepresentable {
                 }
                 
                 guard let results = finishedReq.results as? [VNRecognizedObjectObservation] else { return }
-                if let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) {
                     for observation in results {
-                        let boundingBox = observation.boundingBox
-                        let objectBounds = VNImageRectForNormalizedRect(boundingBox, CVPixelBufferGetWidth(pixelBuffer), CVPixelBufferGetHeight(pixelBuffer))
-                        
                         DispatchQueue.main.async {
-                            // Handle drawing the bounding boxes and labels here
+//                            its showing labels
+                            print(observation.labels)
                         }
                     }
-                }
+                
             }
             
             if let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) {
                 try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+            }
+        }
+        
+        struct CameraView_Previews: PreviewProvider {
+            static var previews: some View {
+                CameraView()
             }
         }
     }
